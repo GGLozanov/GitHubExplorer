889371aaeb47248159353de56648efede9fad72c
GGLozanov <katapultman@abv.bg>
2020-07-01 18:58:17 +0300

    [Update] Added project autogenerated boilerplate

---
+++ GitHubExplorer/GitHubExplorer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubExplorer
+//
+//  Created by ts51 on 1.07.20.
+//  Copyright © 2020 example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
