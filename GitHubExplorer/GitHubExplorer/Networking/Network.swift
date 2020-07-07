9a15809327000e682746297a7d8ac04a38fc9dfc
Ivan Dilchovski <idtechlight@gmail.com>
2020-07-07 12:38:53 +0300

    Add request information to networking logs

--- GitHubExplorer/GitHubExplorer/Networking/Network.swift
+++ GitHubExplorer/GitHubExplorer/Networking/Network.swift
@@ -33,29 +33,29 @@ class Network {
             guard error == nil else {
                 let error = error! as NSError
                 DispatchQueue.main.async { completion(.failure(.cocoaNetworking(error))) }
-                print("[Networking] call failed: \(error.debugDescription)")
+                print("[Networking] \(request.httpMethod ?? "") \(request.url?.absoluteString ?? "call") failed: \(error.debugDescription)")
                 return
             }
 
             guard let response = response as? HTTPURLResponse else {
                 DispatchQueue.main.async { completion(.failure(.noResponse)) }
-                print("[Networking] no response")
+                print("[Networking] \(request.httpMethod ?? "") \(request.url?.absoluteString ?? "call") no response")
                 return
             }
             
             guard response.statusCode >= 200, response.statusCode < 400 else {
                 DispatchQueue.main.async { completion(.failure(.badStatusCode(response.statusCode))) }
-                print("[Networking] Bad response code: \(response.statusCode)")
+                print("[Networking] \(request.httpMethod ?? "") \(request.url?.absoluteString ?? "call") Bad response code: \(response.statusCode)")
                 return
             }
             
             guard let data = data else {
                 DispatchQueue.main.async { completion(.failure(.noData)) }
-                print("[Networking] no data")
+                print("[Networking] \(request.httpMethod ?? "") \(request.url?.absoluteString ?? "call") no data")
                 return
             }
             
-            print("[Networking] SUCCESS!")
+            print("[Networking] \(request.httpMethod ?? "") \(request.url?.absoluteString ?? "call") SUCCESS!")
             DispatchQueue.main.async { completion(.success((data, response))) }
         }.resume() // start the data task
     }
